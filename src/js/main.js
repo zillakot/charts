2507fe9ebaaf71c95cf4ea11cdda61f4cdcfd47c
zillakot <markus.korpinen@gmail.com>
2015-11-03 00:40:36 +0200

    some improvements and webpack config

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,14 @@ import d3 from 'd3';
 import dc from 'dc';
 import nvd3 from 'nvd3';
 import google from 'google-maps';
+import overlayStyle from '../css/overlay.css';
+import dcStyle from '../css/dc.css';
 
-var filename = '0_0_0.json';
+//var filename = '../data/0_0_0.json';
+//var csvfile = '../data/comap.csv';
+
 var testData;
+var stations = [{key: "1", name: "Harmaja", lat: 60.104794, lon: 24.9747871 }];
 
 google.LIBRARIES = ['geometry', 'drawing'];
 
@@ -16,60 +21,103 @@ google.load(google => {
         zoom: 10
     });
 
-    d3.json(filename, pdata => {
+    var pdata = require('../data/0_0_0.json');
+    var data = parseAis(pdata.slice(0,10));
 
-        var data = parseAis(pdata.slice(0,10));
-        //data.forEach(x => {console.log(JSON.stringify(x))});
-        /*var cf = dc.crossfilter(data);
-        var mmsiDim = cf.dimension(x => x.mmsi);
-        var mmsigroup = mmsiDim.group().top(Infinity);*/
+    var overlay = new google.maps.OverlayView();
 
-        var overlay = new google.maps.OverlayView();
+    // Add the container when the overlay is added to the map.
+    overlay.onAdd = function () {
+        var layer = d3.select(this.getPanes().overlayMouseTarget)
+            .append("div")
+            .attr("class", "stations");
 
-        // Add the container when the overlay is added to the map.
-        overlay.onAdd = function () {
-            var layer = d3.select(this.getPanes().overlayLayer)
-                .append("div")
-                .attr("class", "stations");
+        // Draw each marker as a separate SVG element.
+        // We could use a single SVG, but what size would it have?
+        overlay.draw = function () {
+            var projection = this.getProjection();
 
-            // Draw each marker as a separate SVG element.
-            // We could use a single SVG, but what size would it have?
-            overlay.draw = function () {
-                var projection = this.getProjection();
+            var station = layer.selectAll("svg")
+                .data(d3.entries(stations))
+                .each(transform)
+                .enter().append("svg:svg")
+                .each(transform)
+                .attr("class", "station");
 
-                var marker = layer.selectAll("svg")
-                    .data(d3.entries(data))
-                    .each(transform) // update existing markers
-                    .enter().append("svg:svg")
-                    .each(transform)
-                    .attr("class", "marker");
+            station.append("svg:rect")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", 20)
+                .attr("height", 20)
+                .on("click", d => {
+                    asemaDim.filter(d.value.name);
+                    dc.redrawAll();
+                    console.log(d.value.name);
+                    return d;
+                }).on("dblclick", d => {
+                    asemaDim.filterAll();
+                    dc.redrawAll();
+                    console.log(d.value.name);
+                    return d;
+                });
 
-                // Add a circle.
-                marker.append("svg:circle")
-                    .attr("r", 4.5)
-                    .attr("cx", 25)
-                    .attr("cy", 25);
+            station.append("svg:text")
+                .attr("x", 25)
+                .attr("dy", 12)
+                .text(function (d) {
+                    return d.value.name;
+                });
 
-                // Add a label.
-                marker.append("svg:text")
-                    .attr("x", 33)
-                    .attr("dy", 28)
-                    .text(function (d) {
-                        return d.value.mmsi;
-                    });
+            var marker = layer.selectAll("svg")
+                .filter(x => x.value.name != "Harmaja")
+                .data(d3.entries(data))
+                .each(transform) // update existing markers
+                .enter().append("svg:svg")
+                .each(transform)
+                .attr("class", "marker");
 
-                function transform(d) {
-                    d = new google.maps.LatLng(d.value.lat, d.value.lon);
-                    d = projection.fromLatLngToDivPixel(d);
-                    return d3.select(this)
-                        .style("left", (d.x - 25) + "px")
-                        .style("top", (d.y - 25) + "px");
-                }
-            };
+            // Add a circle.
+            marker.append("svg:circle")
+                .attr("r", 8)
+                .attr("cx", 15)
+                .attr("cy", 15)
+                .on("mouseover", expand)
+                .on("mouseout", collapse)
+                .on("click", d => console.log(d.value.lat + " " + d.value.lon));
+
+            // Add a label.
+            marker.append("svg:text")
+                .attr("x", 25)
+                .attr("dy", 20)
+                .text(function (d) {
+                    return d.value.mmsi;
+                });
+
+            function transform(d) {
+                console.log(d.value.lat + " " + d.value.lon);
+                d = new google.maps.LatLng(d.value.lat, d.value.lon);
+                d = projection.fromLatLngToDivPixel(d);
+                return d3.select(this)
+                    .style("left", (d.x - 25) + "px")
+                    .style("top", (d.y - 25) + "px");
+            }
+
+            function expand() {
+                d3.select(this).transition()
+                    .duration(100)
+                    .attr("r",10)
+            }
+
+            function collapse() {
+                d3.select(this).transition()
+                    .duration(100)
+                    .attr("r",8)
+            }
         };
-        // Bind our overlay to the map…
-        overlay.setMap(map);
-    });
+    };
+    // Bind our overlay to the map…
+    overlay.setMap(map);
+
 });
 
 function parseAis(data){
@@ -90,74 +138,71 @@ function parseAis(data){
         });
 */
 
-d3.csv('comap.csv', function(error, pdata) {
+var pdata = require('../data/comap.csv');
+pdata.forEach(function(d){
+    d.rpm = parseFloat(d.rpm);
+    d.kulutus = parseFloat(d.kulutus);
+    d.kayttoaste = parseFloat(d.kayttoaste);
+    d.nopeus = parseFloat(d.nopeus);
+    d.luotsaukset = parseInt(d.luotsaukset);
 
-    pdata.forEach(function(d){
-        d.rpm = parseFloat(d.rpm);
-        d.kulutus = parseFloat(d.kulutus);
-        d.kayttoaste = parseFloat(d.kayttoaste);
-        d.nopeus = parseFloat(d.nopeus);
-        d.luotsaukset = parseInt(d.luotsaukset);
+    return d;
+});
 
-        return d;
+var cf = dc.crossfilter(pdata);
+var all = cf.groupAll();
+
+var asemaDim = cf.dimension(d => d.asema);
+var nimiDim = cf.dimension(d => d.laiva);
+var laivaDim = cf.dimension(d => d.laiva);
+var rpmDim = cf.dimension(d =>d.rpm);
+
+var asemaGroup = asemaDim.group().reduceSum(d => d.luotsaukset);
+
+var rpmGroup = rpmDim.group().reduce(
+    reduceAvgAdd('kulutus'),
+    reduceAvgRemove('kulutus'),
+    reduceAvgInit()
+);
+
+var laivaNopeusGroup = laivaDim.group().reduce(
+    reduceAvgAdd('nopeus'),
+    reduceAvgRemove('nopeus'),
+    reduceAvgInit()
+);
+
+var laivaLuotsauksetGroup = nimiDim.group().reduce(
+    reduceAvgAdd('luotsaukset'),
+    reduceAvgRemove('luotsaukset'),
+    reduceAvgInit()
+);
+
+dc.pieChart("#la")
+    .width(400)
+    .height(200)
+    .dimension(asemaDim)
+    .group(asemaGroup)
+    .innerRadius(50);
+
+barChart(nimiDim, laivaLuotsauksetGroup, '#lv', 'Laiva', 'Luotsaukset')
+    .valueAccessor(p => p.value.avg);
+barChart(laivaDim, laivaNopeusGroup, '#ln', 'Laiva', 'Keskinopeus')
+    .valueAccessor(p => p.value.avg);
+
+barChart(rpmDim, rpmGroup, '#rpm', 'RPM', 'Kulutus')
+    .valueAccessor(p => p.value.sum)
+    .width(1200)
+    .height(200);
+
+dc.dataCount('#data-count')
+    .dimension(cf)
+    .group(all)
+    .html({
+        some: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records',
+        all: 'All records selected. Please click on the graph to apply filters.'
     });
 
-    var cf = dc.crossfilter(pdata);
-    var all = cf.groupAll();
-
-    var asemaDim = cf.dimension(d => d.asema);
-    var nimiDim = cf.dimension(d => d.laiva);
-    var laivaDim = cf.dimension(d => d.laiva);
-    var rpmDim = cf.dimension(d =>d.rpm);
-
-    var asemaGroup = asemaDim.group().reduceSum(d => d.luotsaukset);
-
-    var rpmGroup = rpmDim.group().reduce(
-        reduceAvgAdd('kulutus'),
-        reduceAvgRemove('kulutus'),
-        reduceAvgInit()
-    );
-
-    var laivaNopeusGroup = laivaDim.group().reduce(
-        reduceAvgAdd('nopeus'),
-        reduceAvgRemove('nopeus'),
-        reduceAvgInit()
-    );
-
-    var laivaLuotsauksetGroup = nimiDim.group().reduce(
-        reduceAvgAdd('luotsaukset'),
-        reduceAvgRemove('luotsaukset'),
-        reduceAvgInit()
-    );
-
-    dc.pieChart("#la")
-        .width(400)
-        .height(200)
-        .dimension(asemaDim)
-        .group(asemaGroup)
-        .innerRadius(50);
-
-    barChart(nimiDim, laivaLuotsauksetGroup, '#lv', 'Laiva', 'Luotsaukset')
-        .valueAccessor(p => p.value.avg);
-    barChart(laivaDim, laivaNopeusGroup, '#ln', 'Laiva', 'Keskinopeus')
-        .valueAccessor(p => p.value.avg);
-
-    barChart(rpmDim, rpmGroup, '#rpm', 'RPM', 'Kulutus')
-        .valueAccessor(p => p.value.sum)
-        .width(1200)
-        .height(200);
-
-    dc.dataCount('#data-count')
-        .dimension(cf)
-        .group(all)
-        .html({
-            some: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records',
-            all: 'All records selected. Please click on the graph to apply filters.'
-        });
-
-
-    dc.renderAll();
-});
+dc.renderAll();
 
 function appendLog(data){
     return d3.select('#example')
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@
 module.exports = {
     entry: './src/js/main.js',
     output: {
-        path: './build',
+        path: './dist',
         filename: 'bundle.js'
     },
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            { test: /\.css$/, loader: "style-loader!css-loader" }
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.json$/, loader: "json-loader"},
+            { test: /\.csv$/, loader: "dsv-loader"}
         ]
     }
 };
