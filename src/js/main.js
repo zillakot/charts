cc3086926fef6074a853b18ee49d428f32509330
zillakot <markus.korpinen@gmail.com>
2015-11-06 02:38:37 +0200

    added some css and improved charts

--- src/js/main.js
+++ src/js/main.js
@@ -6,29 +6,32 @@ import google from 'google-maps';
 import overlayStyle from '../css/overlay.css';
 import dcStyle from '../css/dc.css';
 import veneet from '../data/veneet.csv';
+import layout from '../css/layout.css';
 
 veneet.forEach(x => {
     x.mmsi = parseInt(x.mmsi);
-    console.log(JSON.stringify(x))
+    x.kuljettu = parseInt(x.kuljettu);
+    x.huolto = parseInt(x.huolto);
     return x;
 });
-//var filename = '../data/0_0_0.json';
-//var csvfile = '../data/comap.csv';
 
 var testData;
 var stations = [{key: "1", name: "Harmaja", lat: 60.104794, lon: 24.9747871 }];
 
 google.LIBRARIES = ['geometry', 'drawing'];
 
+d3.select("#example").append("input")
+
 google.load(google => {
-    var map = new google.maps.Map(d3.select("#map_in").node(), {
+    var map = new google.maps.Map(d3.select("#map").node(), {
         center: {lat: 60.192, lng: 24.946},
         scrollwheel: true,
-        zoom: 10
+        zoom: 10,
+        sensor: true
     });
 
     var pdata = require('../data/0_0_0.json');
-    var data = parseAis(pdata.slice(0,10));
+    var data = parseAis(pdata.slice(0,100));
 
     var overlay = new google.maps.OverlayView();
 
@@ -85,8 +88,8 @@ google.load(google => {
             // Add a circle.
             marker.append("svg:circle")
                 .attr("r", 8)
-                .attr("cx", 15)
-                .attr("cy", 15)
+                .attr("cx", 10)
+                .attr("cy", 10)
                 .on("mouseover", expand)
                 .on("mouseout", collapse)
                 .on("click", d => console.log(d.value.lat + " " + d.value.lon));
@@ -96,7 +99,7 @@ google.load(google => {
                 .attr("x", 25)
                 .attr("dy", 20)
                 .text(function (d) {
-                    return d.value.mmsi;
+                    return d.value.name;
                 });
 
             function transform(d) {
@@ -133,6 +136,7 @@ function parseAis(data){
         var time = d.nmea.split(" ");
         d.dateTime = new Date(time[2] + " " + time[3] + " " + time[5] + " " + time[4]);
         delete d.nmea;
+        d.name = veneet.filter(x => d.mmsi == x.mmsi)[0].name;
         console.log(JSON.stringify(d));
         return d;
     });
@@ -145,7 +149,6 @@ function parseAis(data){
             center: new goo.LatLng(32.344, 51.048)
         });
 */
-
 var pdata = require('../data/comap.csv');
 pdata.forEach(function(d){
     d.rpm = parseFloat(d.rpm);
@@ -153,7 +156,7 @@ pdata.forEach(function(d){
     d.kayttoaste = parseFloat(d.kayttoaste);
     d.nopeus = parseFloat(d.nopeus);
     d.luotsaukset = parseInt(d.luotsaukset);
-
+    d.mailiaLitra = parseFloat(d.kml)*0.62137;
     return d;
 });
 
@@ -161,46 +164,109 @@ var cf = dc.crossfilter(pdata);
 var all = cf.groupAll();
 
 var asemaDim = cf.dimension(d => d.asema);
-var nimiDim = cf.dimension(d => d.laiva);
-var laivaDim = cf.dimension(d => d.laiva);
+
 var rpmDim = cf.dimension(d =>d.rpm);
+var rpmGroup = rpmDim.group().reduce(...avgReducer('kulutus'));
 
-var asemaGroup = asemaDim.group().reduceSum(d => d.luotsaukset);
+var defaultChartMargins = {top: 10, right: 50, bottom: 30, left: 50};
 
-var rpmGroup = rpmDim.group().reduce(
-    reduceAvgAdd('kulutus'),
-    reduceAvgRemove('kulutus'),
-    reduceAvgInit()
-);
+var boatTripDim = cf.dimension(d => d.laiva);
+var boatTripGroup = boatTripDim.group().reduceSum(d => d.luotsaukset);
 
-var laivaNopeusGroup = laivaDim.group().reduce(
-    reduceAvgAdd('nopeus'),
-    reduceAvgRemove('nopeus'),
-    reduceAvgInit()
-);
+//Chart 1: trips or distance per boat, barchart
+var chart1 = dc.barChart('#chart1')
+    .width(400)
+    .height(300)
+    .margins(defaultChartMargins)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .yAxisLabel("Trips")
+    .xAxisLabel("Boat")
+    .dimension(boatTripDim)
+    .group(boatTripGroup);
 
-var laivaLuotsauksetGroup = nimiDim.group().reduce(
-    reduceAvgAdd('luotsaukset'),
-    reduceAvgRemove('luotsaukset'),
-    reduceAvgInit()
-);
 
-dc.pieChart("#la")
+var boatEffDim = cf.dimension(d => d.laiva);
+var boatEffGroup = boatEffDim.group().reduce(...avgReducer('mailiaLitra'));
+
+
+//chart2: efficiency per boat, barchart
+var chart2 = dc.barChart('#chart2')
+    .width(400)
+    .height(300)
+    .margins(defaultChartMargins)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .yAxisLabel("Efficiency [mi/l]")
+    .xAxisLabel("Boat")
+    .dimension(boatEffDim)
+    .group(boatEffGroup)
+    .valueAccessor(p => p.value.avg);
+
+var veneData = dc.crossfilter(veneet);
+var vall = veneData.groupAll();
+
+var huoltoDim = veneData.dimension(x => x.id);
+var huoltoGroup = huoltoDim.group().reduceSum(d => d.kuljettu);
+
+huoltoDim.filter('L139');
+var chart3 = dc.barChart('#chart3')
+    .width(400)
+    .height(300)
+    .margins(defaultChartMargins)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .yAxisLabel("Overhaul")
+    .xAxisLabel("Boat")
+    .dimension(huoltoDim)
+    .group(huoltoGroup);
+
+var filters = [];
+
+chart2.onClick = (d => {
+    return function() {
+        var a = arguments[0];
+        if (filters.filter(x => x == a.x).length > 0) {filters = filters.filter(x => x != a.x);}
+        else {filters = filters.concat(a.x);}
+        huoltoDim.filter(filters);
+        dc.redrawAll();
+        return  d.apply(this, arguments);
+    }
+})(chart2.onClick);
+
+chart3.onClick = (d => {
+    return function() {
+        log(arguments);
+        return  d.apply(this, arguments);
+    }
+})(chart3.onClick);
+
+//chart4: fault per boat
+//chart5: driving style
+//chart6: efficiency per rpm
+
+var rpmEffDim = cf.dimension(d =>d.rpm);
+var rpmEffGroup = rpmEffDim.group().reduce(...avgReducer('mailiaLitra'));
+
+var chart6 = dc.lineChart('#chart6')
+    .width(1200)
+    .height(300)
+    .margins(defaultChartMargins)
+    .x(d3.scale.linear().domain([600,1900]))
+    .yAxisLabel("Efficiency[mi/l]")
+    .xAxisLabel("RPM")
+    .dimension(rpmEffDim)
+    .group(rpmEffGroup)
+    .valueAccessor(x => x.value.avg);
+
+/*
+dc.pieChart("#chart1")
     .width(400)
     .height(200)
     .dimension(asemaDim)
     .group(asemaGroup)
     .innerRadius(50);
-
-barChart(nimiDim, laivaLuotsauksetGroup, '#lv', 'Laiva', 'Luotsaukset')
-    .valueAccessor(p => p.value.avg);
-barChart(laivaDim, laivaNopeusGroup, '#ln', 'Laiva', 'Keskinopeus')
-    .valueAccessor(p => p.value.avg);
-
-barChart(rpmDim, rpmGroup, '#rpm', 'RPM', 'Kulutus')
-    .valueAccessor(p => p.value.sum)
-    .width(1200)
-    .height(200);
+*/
 
 dc.dataCount('#data-count')
     .dimension(cf)
@@ -235,6 +301,10 @@ function barChart(dim, group, id, xlabel, ylabel, valueAccessor){
     return c;
 }
 
+function avgReducer(key){
+    return [reduceAvgAdd(key), reduceAvgRemove(key), reduceAvgInit()];
+}
+
 function reduceAvgAdd(key){
     return function(p,v){
         ++p.count;
@@ -257,4 +327,8 @@ function reduceAvgInit(){
     return function(){
         return {count: 0, sum: 0, avg: 0};
     }
+}
+
+function log(x){
+    console.log(JSON.stringify(x));
 }
